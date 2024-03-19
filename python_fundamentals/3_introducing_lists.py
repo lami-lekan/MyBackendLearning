b7a3971dee78efeddf52968d017c1bff61c5d93b
lami_lekan <olagbajuisrael11@gmail.com>
2024-03-19 15:07:05 +0000

    python lists

---
+++ python_fundamentals/3_introducing_lists.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+# Creating a list of bicycles brand
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles)
+
+# Accessing elements in a list using their index
+# list index is always off by 1 to it elem position 
+print(bicycles[0])
+print(bicycles[2].title())
+
+# -1 index is the last elem in the list
+print(bicycles[-1])
+
+# changing, adding, and removing elements
