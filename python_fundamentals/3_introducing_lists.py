655bda1e59ee9600cd04ebddb86dd317f6d09bf1
lami_lekan <olagbajuisrael11@gmail.com>
2024-03-29 14:21:44 +0000

    pyhton lists

--- python_fundamentals/3_introducing_lists.py
+++ python_fundamentals/3_introducing_lists.py
@@ -13,3 +13,27 @@ print(bicycles[2].title())
 print(bicycles[-1])
 
 # changing, adding, and removing elements
+motorcycles = ['baja', 'honda', 'yamaha', 'suzuki']
+print(motorcycles)
+motorcycles[1] = 'ducati'
+print(motorcycles)
+motorcycles.append('baja')
+print(motorcycles)
+print()
+phones = []
+phones.append('iphone')
+print(phones)
+phones.append('samsung')
+print(phones)
+phones.insert(1, 'itel')
+print(phones)
+phones.insert(-2, 'nokia')
+print(phones)
+
+del motorcycles[1]
+print(motorcycles)
+popped_phone = phones.pop(1)
+print(phones)
+print(popped_phone)
+del_baja = motorcycles.remove('baja')
+print(motorcycles)
