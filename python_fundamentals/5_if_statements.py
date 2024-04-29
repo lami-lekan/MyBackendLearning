ae086eb9ad0e33a552938188df6352a8d22ca6df
lami_lekan <olagbajuisrael11@gmail.com>
2024-04-29 20:14:44 +0000

    if statement 1

--- python_fundamentals/5_if_statements.py
+++ python_fundamentals/5_if_statements.py
@@ -5,5 +5,26 @@ for car in cars:
         print(car.upper())
     else:
         print(car.title())
+car = 'bmw'
+print(car == 'bmw')
 
+car = 'Audi'
+print(car.lower() == 'audi')
+print(car)
+
+requested_toppings = 'mushrooms'
+
+if requested_toppings != 'anchovies':
+    print('Hold the anchovies')
+
+age_0 = 20
+age_1 = 24
+
+print(age_0 >= 21 and age_1 >= 21)
+print(age_0 >= 21 or age_1 >= 21)
+
+items = ['ball', 'can', 'broom']
+print('broom' in items)
+print('bin' in items)
+print('can' not in items)
 
