371a4737430bb3e2700c974dde67a7616d4e087e
lami_lekan <olagbajuisrael11@gmail.com>
2024-05-11 15:19:58 +0000

    dictionaries 1

--- python_fundamentals/6_dictionaries.py
+++ python_fundamentals/6_dictionaries.py
@@ -26,3 +26,44 @@ alien_1['points'] = 20
 
 print(alien_0, alien_1)
 
+alien_2 = {'color': 'blue', 'x-position': 15, 'y-position': 10, 'speed': 'medium'}
+if alien_2['speed'] == 'slow':
+    alien_2['x-position'] = alien_2['x-position'] + 1
+elif alien_2['speed'] == 'medium':
+    alien_2['x-position'] = alien_2['x-position'] + 2
+else:
+    alien_2['x-position'] = alien_2['x-position'] + 3
+
+print(alien_2['x-position'])
+
+# removing key-val pair
+print(alien_2)
+print('..deleting *color* key/val now >>>...')
+del(alien_2['color'])
+print(alien_2)
+
+favourite_language = {
+    'sarah': 'java',
+    'gibbs': 'python',
+    'noah': 'c',
+    'tim': 'ruby',
+    }
+
+print("Noah's favourite language is " +
+    favourite_language['noah'] + ".")
+
+# looping thru a dictionary
+user_0 = {
+    'username': 'efermi',
+    'first': 'erinco',
+    'last': 'fermi',
+    }
+
+for key, value in user_0.items():
+    print("\n key: " + key)
+    print("value: " + value)
+
+for name in favourite_language.keys():
+    print(name)
+
+
