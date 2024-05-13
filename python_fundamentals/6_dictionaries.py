58c04daa7c20203385bb98438b26db6308e5b5da
lami_lekan <olagbajuisrael11@gmail.com>
2024-05-13 10:18:48 +0000

    dictionaries 2

--- python_fundamentals/6_dictionaries.py
+++ python_fundamentals/6_dictionaries.py
@@ -66,4 +66,31 @@ for key, value in user_0.items():
 for name in favourite_language.keys():
     print(name)
 
+for name in sorted(favourite_language):
+    print('\n' + name)
+print("\nThese are the list of available programing language: ")
+for language in favourite_language.values():
+    print(language)
 
+# nesting
+
+alien_0 = {'color': 'green', 'points': 5}
+alien_1 = {'color': 'yellow', 'points': 10}
+alien_2 = {'color': 'red', 'points': 15}
+
+aliens = [alien_0, alien_1, alien_2]
+for alien in aliens:
+    print(alien)
+
+
+aliens = []
+
+for alien_number in range(30):
+    new_alien = {'color': 'green', 'points': 5}
+    aliens.append(new_alien)
+
+for alien in aliens[:5]:
+    print(alien)
+print('....')
+
+print(len(aliens))
