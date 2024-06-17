172f02260c61bb65c9c73e6d4b4d43fb1843ab9a
lami_lekan <olagbajuisrael11@gmail.com>
2024-06-17 16:38:44 +0000

    dictionaries 3

--- python_fundamentals/6_dictionaries.py
+++ python_fundamentals/6_dictionaries.py
@@ -94,3 +94,33 @@ for alien in aliens[:5]:
 print('....')
 
 print(len(aliens))
+
+pizza = {
+    'crust': 'thick',
+    'toppings': ['mushroom', 'extra cheese'],
+    }
+print('You ordered a ' + pizza['crust'] + '-crust pizza with the following toppings: ')
+
+for topping in pizza['toppings']:
+    print(topping)
+
+users = {
+    'aeinstein':{
+        'first': 'albert',
+        'last': 'einstein',
+        'location': 'princeton',
+        },
+    'mcurie':{
+        'first': 'marie',
+        'last': 'curie',
+        'location': 'paris',
+        }
+    }
+
+for username, user_info in users.items():
+    print("\nUsername: " + username)
+    full_name = user_info['first'] + " " + user_info['last']
+    location = user_info['location']
+
+    print("\tFull name: " + full_name)
+    print("\tLocation: " + location)
